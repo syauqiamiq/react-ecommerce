564ea2cb9e2fd9b8da12969d9cb6dc46fc497e95
Muhammad Syauqi Amiq Amrullah <syauqiamiq12@gmail.com>
2021-11-24 09:06:54 +0700

    CSS Settings

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const HomePage = () => {
   return (
     <>
-      <h1>This is Homepage</h1>
+      <h1 className="animate__animated animate__bounce">This is Homepage</h1>
     </>
   )
 }
