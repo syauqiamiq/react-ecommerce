c4a66245d9abea5f071cdd22a7376417e1f98aee
Muhammad Syauqi Amiq Amrullah <syauqiamiq12@gmail.com>
2021-12-08 08:39:03 +0700

    Add: Axios package and Consume GetVisitor API

---
+++ src/api/api.jsx
@@ -0,0 +1,5 @@
+class APIServices {
+  static baseURL = "http://127.0.0.1:8000/api";
+  static getVisitiroDetails = this.baseURL + "/getvisitor";
+}
+export default APIServices;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Categories from "../components/home/Categories";
 import Collection from "../components/home/Collection";
 import FeaturedProducts from "../components/home/FeaturedProducts";
 import NewArrival from "../components/home/NewArrival";
 import HomeLayout from "./HomeLayout";
+import axios from "axios";
+import APIServices from "../api/api";
 
 const HomePage = () => {
+  //GET VISITOR DETAIL
+  // useEffect(() => {
+  //   axios
+  //     .get(APIServices.getVisitiroDetails)
+  //     .then((response) => {
+  //       // handle success
+  //       console.log(response.data);
+  //     })
+  //     .catch((error) => {
+  //       // handle error
+  //       console.log(error);
+  //     });
+  // }, []);
   return (
     <HomeLayout>
       <FeaturedProducts />
