623ec7081e8919ab58e43a10a322a5fb63c9d74f
Muhammad Syauqi Amiq Amrullah <syauqiamiq12@gmail.com>
2021-12-08 10:12:23 +0700

    Add: Some Dependency and Consume Some Site Info  API

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -1,5 +1,6 @@
 class APIServices {
   static baseURL = "http://127.0.0.1:8000/api";
   static getVisitiroDetails = this.baseURL + "/getvisitor";
+  static getAllSiteInfo = this.baseURL + "/allsiteinfo";
 }
 export default APIServices;
